2accba65c60fc48cabee313e598088ef35b3e3c7
SarahVanT <sarahvantilburg1@outlook.com>
2024-02-28 10:05:32 -0500

    Cleaned up homepage and now working on about me page

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  const offset = 300; // Pixels before the element is in view
-  return (
-    rect.top >= 0 - offset &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) + offset &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-document.addEventListener("scroll", function () {
-  const aboutMeText = document.querySelector(".about-me-text");
-  const aboutMePaintings = document.querySelector(".about-me-paintings");
-
-  if (isInViewport(aboutMeText)) {
-    aboutMeText.classList.add("enter-left");
-  }
-
-  if (isInViewport(aboutMePaintings)) {
-    aboutMePaintings.classList.add("enter-right");
-  }
-});
-
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".img-box").classList.add("enter-right");
   document.querySelector(".text-box").classList.add("enter-left");
